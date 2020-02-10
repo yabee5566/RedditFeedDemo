6a9f189a445c0ecb59bb5edc7b7917ce1d5e241f
Ian Pan <>
2020-02-10 16:23:21 +0800

    Add two style of custom TableViewCell in tableView
    TODO:
    Set correct UI design for tableView

---
+++ RedditFeedDemo/RedditFeedDemo/Feeds/View/FeedTableCell/RegularTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  RegularTableCell.swift
+//  RedditFeedDemo
+//
+//  Created by Ian Pan on 2020/2/10.
+//  Copyright © 2020 Ian Pan. All rights reserved.
+//
+
+import UIKit
+
+class RegularTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- RedditFeedDemo/RedditFeedDemo/Feeds/ViewController/FeedViewController.swift
+++ RedditFeedDemo/RedditFeedDemo/Feeds/ViewController/FeedViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.register(UINib(nibName: "FullGraphTableCell", bundle: nil), forCellReuseIdentifier: "FullGraphTableCell")
+        self.tableView.register(UINib(nibName: "RegularTableCell", bundle: nil), forCellReuseIdentifier: "RegularTableCell")
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
+    // Test custom tableview with 2 style tableViewCell
+    //TODO: Replace this
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row % 2 == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FullGraphTableCell") as! FullGraphTableCell
+            // Set up cell.label
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RegularTableCell") as! RegularTableCell
+            // Set up cell.button
+            return cell
+        }
+    }
+
+    // Test custom tableview with 2 height
+    //TODO: Replace this
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row % 2 == 0 {
+            return 200
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RegularTableCell") as! RegularTableCell
+            // Set up cell.button
+            return 100
+        }
+    }
 }
 
